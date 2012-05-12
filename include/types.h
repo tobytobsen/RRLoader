8f654110096e9fe17f8e7a77f22a771c475f33c6
teslaNova <terranova@fahr-zur-hoelle.org>
2012-05-12 22:59:50 +0200

    sync and async accept, unfinished

--- include/types.h
+++ include/types.h
@@ -1,9 +1,16 @@
 #ifndef TYPES_H_
 #define TYPES_H_
 
+#include <stdbool.h>
+
+#define __in
+#define __out
+#define __inout
+
 typedef unsigned char			uint8_t;
 typedef unsigned short			uint16_t;
 typedef unsigned int			uint32_t;
 typedef unsigned long long int 	uint64_t;
+
 
 #endif /* TYPES_H_ */
