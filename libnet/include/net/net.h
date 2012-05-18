cdd2a7e34e1b265551c8d3f0d7508bbc22dd3cf5
teslaNova <terranova@fahr-zur-hoelle.org>
2012-05-18 15:24:09 +0200

    implemented error codes and strings to work with

--- libnet/include/net/net.h
+++ libnet/include/net/net.h
@@ -1,6 +1,53 @@
 #ifndef LIBNET_NET_H_
 #define LIBNET_NET_H_
 
+#include <types.h>
+
 #define LIBNET_UNSUPPORTED (unsigned int)(-1)
+
+void
+libnet_error_set(uint32_t err);
+
+uint32_t
+libnet_error_get(void);
+
+const char*
+libnet_str_error(uint32_t err);
+
+enum {
+	LIBNET_E_NONE = 0,
+
+	/* ARGS */
+	LIBNET_E_INV_ARG,
+
+	/* PROTOCOL */
+	LIBNET_E_IPV6_NOT_SUPPORTED = 100,
+	LIBNET_E_IPV6_ONLY_FAILED,
+	
+	/* LIBRARY */
+	LIBNET_E_CONNECT_FAILED = 200,
+	LIBNET_E_BIND_FAILED,
+	LIBNET_E_LISTEN_FAILED,
+	LIBNET_E_RECV_FAILED,
+	LIBNET_E_SEND_FAILED,
+	LIBNET_E_RESOLVE_HOST,
+	
+	/* INTERNAL */
+	LIBNET_E_SET_SIZE_EXCEEDED = 300,
+
+	/* SSL */
+	LIBNET_E_ENC_SHUTDOWN = 400,
+	LIBNET_E_ENC_NEW,
+	LIBNET_E_ENC_CONNECT,
+	LIBNET_E_ENC_ACCEPT,
+	LIBNET_E_ENC_SSL_CTX,
+	LIBNET_E_ENC_SSL_CA_CERT,
+	LIBNET_E_ENC_SSL_CERT,
+	LIBNET_E_ENC_SSL_KEY,
+
+	/* PLATFORM */
+	LIBNET_E_W32_STARTUP = 500,
+	LIBNET_E_W32_CLEANUP,
+};
 
 #endif /* LIBNET_NET_H_ */
---
+++ libnet/net.c
@@ -0,0 +1,66 @@
+#include <net/net.h>
+
+
+static
+uint32_t error = LIBNET_E_NONE;
+
+void
+libnet_error_set(uint32_t err) {
+	error = err;
+}
+
+uint32_t
+libnet_error_get(void) {
+	return error;
+}
+
+const char*
+libnet_str_error(uint32_t err) {
+	switch(err) {
+		default: return "unknown error";
+
+		case LIBNET_E_NONE: 
+			return "no error occurred";
+
+		case LIBNET_E_INV_ARG: 
+			return "invalid argument delivered";
+		
+		case LIBNET_E_IPV6_NOT_SUPPORTED: 
+			return "IPv6 is not supported yet";
+		case LIBNET_E_IPV6_ONLY_FAILED: 
+			return "IPv6 only address lookup failed";
+		
+		case LIBNET_E_CONNECT_FAILED: 
+			return "failed to connect to host";
+		case LIBNET_E_BIND_FAILED: 
+			return "failed to bind socket to desired port";
+		case LIBNET_E_LISTEN_FAILED: 
+			return "failed to listen";
+		case LIBNET_E_RECV_FAILED: 
+			return "failed to read from socket";
+		case LIBNET_E_SEND_FAILED: 
+			return "failed to send to destination";
+		case LIBNET_E_RESOLVE_HOST: 
+			return "failed to resolve hostname";
+
+		case LIBNET_E_SET_SIZE_EXCEEDED: 
+			return "socket set is full";
+
+		case LIBNET_E_ENC_SHUTDOWN: 
+			return "failed to shutdown encrypted socket";
+		case LIBNET_E_ENC_NEW: 
+			return "failed to create encrypted socket";
+		case LIBNET_E_ENC_CONNECT: 
+			return "failed to encrypt connection";
+		case LIBNET_E_ENC_ACCEPT: 
+			return "failed to accept client through encrypted connection";
+		case LIBNET_E_ENC_SSL_CTX: 
+			return "failed to create ssl context";
+		case LIBNET_E_ENC_SSL_CA_CERT: 
+			return "invalid ssl ca certificate";
+		case LIBNET_E_ENC_SSL_CERT: 
+			return "invalid ssl certificate";
+		case LIBNET_E_ENC_SSL_KEY: 
+			return "invalid ssl key";
+	}
+}
