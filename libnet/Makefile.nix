c791c16ad8a63e78aeb92cb6839abf1c8d76a7d5
teslaNova <terranova@fahr-zur-hoelle.org>
2012-05-18 14:57:48 +0200

    added simple ssl example

--- libnet/Makefile.nix
+++ libnet/Makefile.nix
@@ -5,40 +5,47 @@ TARGET=bin/libnet
 SRC=$(shell find . -depth 1 -name '*.[cS]')
 OBJ=$(addsuffix .o, $(subst %, %, $(basename $(SRC))))
 
-CFLAGS+= -I./include/ -I../include/
+CFLAGS+= -I./include/ -I../include/ -I../ext/include -fnested-functions
+LDFLAGS+= -lnet -lcrypto -lssl
 
 static: $(OBJ)
-	$(AR) $(ARFLAGS) $(TARGET).a $(OBJ)
+	$(AR) $(ARFLAGS) $(TARGET).a $(OBJ) ../ext/lib/darwin-x86_64/libcrypto.a ../ext/lib/darwin-x86_64/libssl.a
 
 shared:
 	@echo Shared Library is not supported yet
 
 test: static
-	$(CC) $(CFLAGS) tests/tcp_client_sync.c -o tests/tcp_client_sync -L"bin" -lnet
+	$(CC) $(CFLAGS) $(LDFLAGS) tests/tcp_client_sync.c -o tests/tcp_client_sync.test -L"bin"
 	
-	$(CC) $(CFLAGS) tests/tcp_ipv6_client_sync.c -o tests/tcp_ipv6_client_sync -L"bin" -lnet
+	$(CC) $(CFLAGS) $(LDFLAGS) tests/tcp_ipv6_client_sync.c -o tests/tcp_ipv6_client_sync.test -L"bin"
 
-	$(CC) $(CFLAGS) tests/tcp_client_async.c -o tests/tcp_client_async -L"bin" -lnet
+	$(CC) $(CFLAGS) $(LDFLAGS) tests/tcp_client_async.c -o tests/tcp_client_async.test -L"bin" -lnet
 
-	$(CC) $(CFLAGS) tests/tcp_simple_server_sync.c -o tests/tcp_simple_server_sync -L"bin" -lnet
-	$(CC) $(CFLAGS) tests/tcp_simple_client_sync.c -o tests/tcp_simple_client_sync -L"bin" -lnet
+	$(CC) $(CFLAGS) $(LDFLAGS) tests/tcp_simple_server_sync.c -o tests/tcp_simple_server_sync.test -L"bin"
+	$(CC) $(CFLAGS) $(LDFLAGS) tests/tcp_simple_client_sync.c -o tests/tcp_simple_client_sync.test -L"bin"
 
-	$(CC) $(CFLAGS) tests/tcp_ipv6_simple_server_sync.c -o tests/tcp_ipv6_simple_server_sync -L"bin" -lnet
-	$(CC) $(CFLAGS) tests/tcp_ipv6_simple_client_sync.c -o tests/tcp_ipv6_simple_client_sync -L"bin" -lnet
+	$(CC) $(CFLAGS) $(LDFLAGS) tests/tcp_ipv6_simple_server_sync.c -o tests/tcp_ipv6_simple_server_sync.test -L"bin"
+	$(CC) $(CFLAGS) $(LDFLAGS) tests/tcp_ipv6_simple_client_sync.c -o tests/tcp_ipv6_simple_client_sync.test -L"bin"
+
+	$(CC) $(CFLAGS) $(LDFLAGS) tests/udp_client_sync.c -o tests/udp_client_sync.test -L"bin" 
+
+	$(CC) $(CFLAGS) $(LDFLAGS) tests/tcp_client_sync_ssl.c -o tests/tcp_client_sync_ssl.test -L"bin"
 
 
-#	@exec ./tests/tcp_client_sync > ./tests/tcp_client_sync.log &
-#	@exec ./tests/tcp_ipv6_client_sync > ./tests/tcp_ipv6_client_sync.log &
-#	@exec ./tests/tcp_client_async > ./tests/tcp_client_async.log &
-#	@exec ./tests/tcp_simple_server_sync > ./tests/tcp_simple_server_sync.log &
-#	@exec ./tests/tcp_simple_client_sync > ./tests/tcp_simple_client_sync.log &
+#	@exec ./tests/tcp_client_sync.test > ./tests/tcp_client_sync.log &
+#	@exec ./tests/tcp_ipv6_client_sync.test > ./tests/tcp_ipv6_client_sync.log &
+#	@exec ./tests/tcp_client_async.test > ./tests/tcp_client_async.log &
+#	@exec ./tests/tcp_simple_server_sync.test > ./tests/tcp_simple_server_sync.log &
+#	@exec ./tests/tcp_simple_client_sync.test > ./tests/tcp_simple_client_sync.log &
 #
-#	@exec ./tests/tcp_ipv6_simple_server_sync > ./tests/tcp_ipv6_simple_server_sync.log &
-#	@exec ./tests/tcp_ipv6_client_sync > ./tests/tcp_ipv6_client_sync.log &
+#	@exec ./tests/tcp_ipv6_simple_server_sync.test > ./tests/tcp_ipv6_simple_server_sync.log &
+#	@exec ./tests/tcp_ipv6_client_sync.test > ./tests/tcp_ipv6_client_sync.log &
 
 clean:
-	$(RM) $(OBJ)
-	$(RM) $(TARGET).*
+	$(RM) -f $(OBJ)
+	$(RM) -f $(TARGET).*
+	$(RM) -f tests/*.log
+	$(RM) -f tests/*.test
 
 %.o: %.c
 	$(CC) $(CFLAGS) -c $^ -o $@
---
+++ libnet/tests/tcp_client_sync_ssl.c
@@ -0,0 +1,46 @@
+#include <net/net.h>
+#include <net/socket.h>
+
+#include <stdio.h>
+#include <string.h>
+
+#define HOSTNAME	"www.google.de"
+#define PORT		443
+
+#define SSL_CERT	"tests/ssl/ca.crt"
+#define SSL_KEY		"tests/ssl/ca.key"
+
+int main(void) {
+	socket_t s;
+	uint8_t buf[4096] = {0};
+
+	uint8_t req[] = "GET /\r\nHTTP/1.1\r\n\r\n";
+
+	if(false == socket_create_socket(&s, LIBNET_PROTOCOL_TCP, LIBNET_IPV4)) {
+		printf("failed to create socket\r\n");
+		return -1;
+	}
+
+	if(false == socket_set_encryption(&s, LIBNET_ENC_SSL_V3, SSL_CERT, SSL_KEY)) {
+		printf("unable to use SSLv3 encryption\r\n");
+	}
+
+	if(false == socket_connect(&s, HOSTNAME, PORT)) {
+		printf("failed to connect to %s:%d\r\n", HOSTNAME, PORT);
+	} else {
+		printf("connected to %s : %d\r\nSending HTTP Request..\r\n", HOSTNAME, PORT);
+	}
+
+	socket_write(&s, req, strlen((char*)req));
+
+	if(0 < socket_read(&s, buf, 4096)) {
+		printf("read: %s", buf);
+	}
+
+	socket_disconnect(&s);
+	socket_release_socket(&s);
+
+	printf("Latest error: %d (%s)\n", libnet_error_get(), libnet_str_error(libnet_error_get()));
+
+	return 0;
+}
