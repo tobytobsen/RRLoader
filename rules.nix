9dd5bef5b2ce44e8de8c2cd3c9eba224d589bd2d
teslaNova <terranova@fahr-zur-hoelle.org>
2012-05-19 13:10:17 +0200

    doxygen added

--- rules.nix
+++ rules.nix
@@ -1,6 +1,6 @@
 # evtl. die Pfade mit `which <prog>` lokalisieren lassen
 CC=/usr/bin/gcc
-LD=/usr/local/cross/bin/i586-elf-ld
+LD=/usr/bin/ld
 LS=/bin/ls
 MV=/bin/mv
 CP=/bin/cp
@@ -9,7 +9,13 @@ AR=/usr/bin/ar
 NASM=/usr/bin/nasm
 MAKE=/usr/bin/make
 
+DOX=`which doxygen`
+
 ARFLAGS=rcs
 LDFLAGS=-L/usr/local/cross/lib
 CFLAGS=-fleading-underscore 	\
 		-std=c99
+
+
+# SETTINGS
+GEN_DOC=0
