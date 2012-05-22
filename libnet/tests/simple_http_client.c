c0465c19d24e23507b0d14682bf12a324ef618fd
teslaNova <terranova@fahr-zur-hoelle.org>
2012-05-22 20:02:50 +0200

    added callbacks

--- libnet/tests/simple_http_client.c
+++ libnet/tests/simple_http_client.c
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 
+void http_read(uint32_t rid, char *data, uint32_t len) {
+	printf("Request #%d: received %d bytes\n", rid, len);
+	//printf("%s", data);
+}
+
 int main(void) {
 	http_response_t res;
 	http_request_t req;
@@ -14,12 +19,13 @@ int main(void) {
 	}
 
 	http_request_create(&con, &req, LIBNET_HTTP_V11, LIBNET_HTTP_GET, "/");
+	http_request_set_callback(&req, LIBNET_HTTP_CBT_READ, (void *)http_read);
 	http_request_exec(&con, &req, &res);
 	http_request_release(&req);
 
-	printf("Request sent. Response: %d\r\n", res.code);
-
 	http_disconnect(&con);
+
+	printf("\r\nLast Error: %s (%d)\n", libnet_str_error(libnet_error_get()), libnet_error_get());
 
 	return 0;
 }
