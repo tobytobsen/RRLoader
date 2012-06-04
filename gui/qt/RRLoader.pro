db097304d92d2d1162cb554e1f3a152d2b803287
Rushh0ur <syserr@gmx.de>
2012-06-04 23:53:17 +0200

    implemented application single instance; mask, center and move window

--- gui/qt/RRLoader.pro
+++ gui/qt/RRLoader.pro
@@ -3,8 +3,18 @@ TARGET = RRLoader
 QT += core \
     gui \
     network
-HEADERS += src/qrrmainwin.h
-SOURCES += src/qrrmainwin.cpp \
+HEADERS += src/QtSingleApplication/qtlocalpeer.h \
+    src/QtSingleApplication/qtlockedfile.h \
+    src/QtSingleApplication/qtsingleapplication.h \
+    src/QtSingleApplication/qtsinglecoreapplication.h \
+    src/qrrmainwin.h
+SOURCES += src/QtSingleApplication/qtlocalpeer.cpp \
+    src/QtSingleApplication/qtlockedfile.cpp \
+    src/QtSingleApplication/qtlockedfile_unix.cpp \
+    src/QtSingleApplication/qtlockedfile_win.cpp \
+    src/QtSingleApplication/qtsingleapplication.cpp \
+    src/QtSingleApplication/qtsinglecoreapplication.cpp \
+    src/qrrmainwin.cpp \
     src/main.cpp
 FORMS += src/qrrmainwin.ui
 RESOURCES += 
--- gui/qt/src/qrrmainwin.h
+++ gui/qt/src/qrrmainwin.h
@@ -2,18 +2,39 @@
 #define QRRMAINWIN_H
 
 #include <QtGui/QMainWindow>
+#include <QDesktopWidget>
+#include <QMouseEvent>
 #include "ui_qrrmainwin.h"
 
 class QRRMainWin : public QMainWindow
 {
     Q_OBJECT
 
-public:
-    QRRMainWin(QWidget *parent = 0);
-    ~QRRMainWin();
+	public:
+		QRRMainWin(QWidget *parent = 0);
+		~QRRMainWin();
 
-private:
-    Ui::QRRMainWinClass ui;
+	protected:
+		void resizeEvent ( QResizeEvent * event );
+
+	private:
+		Ui::QRRMainWinClass ui;
+
+	public slots:
+		void messageFromSecondInstance(const QString &message);
+
+#if defined(Q_OS_WIN)
+
+	protected:
+		void mousePressEvent(QMouseEvent *event);
+		void mouseMoveEvent(QMouseEvent *event);
+		void mouseReleaseEvent(QMouseEvent *event);
+	private:
+		QPoint offset;
+		bool moving;
+
+#endif
+
 };
 
 #endif // QRRMAINWIN_H
