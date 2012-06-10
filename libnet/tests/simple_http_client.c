7a129aa0e1891dd2b849a0d623d4955bc3720356
teslaNova <terranova@fahr-zur-hoelle.org>
2012-06-10 18:34:15 +0200

    simple http client (rr:board, discussion forums [as param])

--- libnet/tests/simple_http_client.c
+++ libnet/tests/simple_http_client.c
@@ -1,29 +1,21 @@
 #include <net/net.h>
 #include <net/http.h>
+#include <net/http_header.h>
+#include <net/http_options.h>
 
 #include <stdio.h>
 
-void http_read(uint32_t rid, char *data, uint32_t len) {
-	printf("Request #%d: received %d bytes\n", rid, len);
-	printf("%s", data);
-}
-
 int main(void) {
-	http_response_t res;
-	http_request_t req;
-	http_con_t con;
+	http_ctx_t ctx;
 
-	if(false == http_connect(&con, "http://www.google.de", LIBNET_HTTP_V11)) {
+	if(false == http_connect(&ctx, "http://board.raidrush.ws/forumdisplay.php")) {
 		printf("unable to connect to remote host.\r\n");
 		return -1;
 	}
 
-	http_request_create(&con, &req, LIBNET_HTTP_GET, "/");
-	http_request_set_callback(&req, LIBNET_HTTP_CBT_READ, (void *)http_read);
-	http_request_exec(&con, &req, &res);
-	http_request_release(&req);
-
-	http_disconnect(&con);
+	http_option_set(&ctx, LIBNET_HTTP_OPT_PARAM, "f=13");
+	http_execute(&ctx);
+	http_disconnect(&ctx);
 
 	printf("\r\nLast Error: %s (%d)\n", libnet_str_error(libnet_error_get()), libnet_error_get());
 
